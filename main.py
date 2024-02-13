de4ca6e33382b0dc71493b6d6d7098364ca10db4
Theo MG <temates@me.com>
2024-02-13 13:07:56 +0100

    13:07

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
-MAX_LINES = 3 # Global constant
+# Global constant
+MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
+ENTER_NUMBER_MSG = 'Please enter a number'
+
 
 def deposit():
     while True:
         amount = input('What would you like to deposite? $')
         if amount.isdigit():
             amount = int(amount)
-            if amount > 0: break
-            else: print('Amount must be greater than 0')
-        else: print('Please enter a number')
+            if amount > 0:
+                break
+            else:
+                print('Amount must be greater than 0')
+        else:
+            print(ENTER_NUMBER_MSG)
+    return amount
+
+
+def get_bet():
+    while True:
+        amount = input('What would you like to bet on each line? $')
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print('Enter a valid number')
+        else:
+            print(ENTER_NUMBER_MSG)
     return amount
 
 
@@ -16,14 +38,30 @@ def get_number_of_lines():
         lines = input(f'Enter the number of lines to bet on (1-{MAX_LINES})?')
         if lines.isdigit():
             lines = int(lines)
-            if 1<= lines <= MAX_LINES: break
-            else: print('Enter a valid number of lines')
-        else: print('Please enter a number')
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print('Enter a valid number of lines')
+        else:
+            print(ENTER_NUMBER_MSG)
     return lines
+
 
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    print(balance, lines)
+
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet > balance:
+            print(
+                f"Sorry, you don't have enough credit, current balance: ${balance}")
+        else:
+            break
+
+    print(f"You're betting ${bet} on {lines} lines. Total bet: ${total_bet}")
+
 
 main()
